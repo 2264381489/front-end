dd814c57468771f668f06849d5910ff1d229f03d
yanzezhong <36435002+2264381489@users.noreply.github.com>
2019-09-29 23:47:08 +0800

    这个就是进行的前端学习内容更新

--- js-colonel.js
+++ js-colonel.js
@@ -1,1 +1,23 @@
-var sNum1='';
+var sNum1='';//设置一个用来中转的数据
+var sOpr='';//设置一个用来存储符号的变量
+var reflesh=false;//用来表示刷新的
+//制作运算控制器
+function suan(iNum1,iNum2,s0p1) {
+    var iReault=0;
+    switch (sOpr) {
+        case '+':
+           iNum1=iNum1+iNum2;
+           break;
+        case '-':
+            iReault=iNum1-iNum2;
+            break;
+        case "×":
+            iReault=iNum1*iNum2;
+        case "÷":
+            iReault=iNum1/iNum2;
+
+
+    }
+
+
+}
